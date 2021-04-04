adb68e5bab8b36a894be946891ef4ff2bb12fb48
Florent Dufour <florent.dufour@lrz.de>
2021-04-04 11:12:02 +0200

    Solution added

---
+++ 2-Neural-network/question/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+jupyter notebook --allow-root --notebook-dir=. --ip=0.0.0.0 --port=8888 --no-browser
---
+++ 2-Neural-network/solution/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This is the command to be executed when the container starts
+
+jupyter notebook --allow-root --notebook-dir=. --ip=0.0.0.0 --port=8888 --no-browser
