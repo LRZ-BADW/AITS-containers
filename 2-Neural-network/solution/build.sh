8c3eeb55ccf52a2722c0fa5b2cdf344f8cbbf901
Florent Dufour <florent.dufour@lrz.de>
2021-04-07 11:05:18 +0200

    Scripts use Docker instead of docker-compose

--- 2-Neural-network/solution/build.sh
+++ 2-Neural-network/solution/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-eval docker-compose build
+docker build . -t "ann-dream-solution"
--- 2-Neural-network/solution/run.sh
+++ 2-Neural-network/solution/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-eval docker-compose up
+docker run --rm -p 8888:8888 -v images:/root/notebooks/images --hostname ann ann-dream-solution
